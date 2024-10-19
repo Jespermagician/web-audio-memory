b4a89acbf3042c98d958e2803c8dc7e4183b8e86
jespermagician <jesper@herlings.de>
2024-10-19 23:30:03 +0200

    Code Dynamic Base Soundboard

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,29 @@
-const sounds = ['audio1', 'audio2', 'audio3', 'audio4'];
-
+const sounds = [
+    ['audio1', 'audio\\Pink-Panther-Theme.mp3'], 
+    ['audio2', 'audio\\martin-halts-maul.mp3'],
+    ['audio3', 'audio\\Jugendwort-2022.mp3'],
+    ['audio4', 'audio\\Ronaldo-Shoppe.mp3'],
+    ['audio5', 'audio\\Sternenstaub-schmidt-rin.mp3']
+    ];
 
 // later let the audios create it self as well
+sounds.forEach((sounds, index) => {
+    const audioField = document.createElement('audio');
+    audioField.id = sounds[0];
+    audioField.src = sounds[1];
+    document.getElementById('audiosDiv').appendChild(audioField);
+})
 
 sounds.forEach((sounds) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
 
-    btn.innerText = sounds;
+    btn.innerText = sounds[0]; // hier allgemeiner text einfügen
+
+    btn.addEventListener('click', ()=>{
+        console.log(sounds[0])
+        document.getElementById(sounds[0]).play();
+    });
 
     document.getElementById('buttons').appendChild(btn);
 } )
