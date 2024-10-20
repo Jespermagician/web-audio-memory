292fe6b96d94a7550d499eef30715d947895f923
jespermagician <jesper@herlings.de>
2024-10-20 22:13:01 +0200

    Add Pause click and re click. Click Action. Better code etc

--- myscripts.js
+++ myscripts.js
@@ -4,29 +4,117 @@ const sounds = [
     ['audio3', 'audio\\Jugendwort-2022.mp3'],
     ['audio4', 'audio\\Ronaldo-Shoppe.mp3'],
     ['audio5', 'audio\\Sternenstaub-schmidt-rin.mp3']
-    ];
+];
+
+
+const ButtonTitleName = "Guess";
+
+const AudioIdName = 'Audio';
+const AudioIdCloneName = 'c';
+
+const pairsAmount = sounds.length;
+var Found = 0;
+var Pairs = [];
+var selcted = false;
+
+var CurrentSongId = sounds[0][0];
+var MusicPlay = false;
+console.log(CurrentSongId)
 
 // later let the audios create it self as well
 sounds.forEach((sounds, index) => {
-    const audioField = document.createElement('audio');
-    audioField.id = sounds[0];
-    audioField.src = sounds[1];
-    document.getElementById('audiosDiv').appendChild(audioField);
+    Pairs.push(
+    [
+        CreateAudio(AudioIdName + index, sounds[1]),
+        CreateAudio(AudioIdName + index + AudioIdCloneName, sounds[1])
+    ]);
 })
 
-sounds.forEach((sounds) => {
+var buttonArr = []
+sounds.forEach((sounds, index) => {
+    
+    CreateButton(AudioIdName + index)
+    CreateButton(AudioIdName + index + AudioIdCloneName)
+} )
+
+buttonArr = shuffleArray(buttonArr)
+console.log(buttonArr)
+
+// functions
+
+function CreateAudio(id, path)
+{
+    const audioField = document.createElement('audio');
+    audioField.id = id;
+    console.log(audioField.id)
+    audioField.src = path;
+    document.getElementById('audiosDiv').appendChild(audioField);
+    return id;
+}
+
+
+function CreateButton(_audioId)
+{
+    // das hier zusammen fassen mit Methode, wie oben
     const btn = document.createElement('button');
     btn.classList.add('btn');
 
-    btn.innerText = sounds[0]; // hier allgemeiner text einfügen
+    btn.innerText = ButtonTitleName;
 
-    btn.addEventListener('click', ()=>{
-        console.log(sounds[0])
-        document.getElementById(sounds[0]).play();
+    btn.addEventListener('click', (e)=>{
+        stopSong();
+        // Wenn selber Audio ist am laufen, spiele nicht erneut
+        if(CurrentSongId != _audioId || !MusicPlay)
+        {
+            CurrentSongId = _audioId;
+            document.getElementById(CurrentSongId).play();
+            MusicPlay = true;
+            console.log(CurrentSongId + ", play");
+            if(!selcted)
+            {
+                const el = e.currentTarget;
+                el.style.backgroundColor  = 'rgb(109, 59, 59)';
+            }
+        }
+        else
+        {
+            console.log(CurrentSongId + ", is already on play");
+            MusicPlay = false;
+        }
+        
     });
 
-    document.getElementById('buttons').appendChild(btn);
-} )
+    buttonArr.push(btn)
+}
+
+
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+buttonArr.forEach((buttonArr) => {
+    console.log(buttonArr)
+    document.getElementById('buttons').appendChild(buttonArr);
+});
+
+
+function stopSong()
+{
+    if (CurrentSongId != null && MusicPlay)
+    {
+        let songs = document.getElementById(CurrentSongId);
+        songs.pause();
+        console.log(CurrentSongId);
+        songs.currentTime = 0;  
+    }
+}
 
 
 
